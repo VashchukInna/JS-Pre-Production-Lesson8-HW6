7ef1b649166a0e08017008e82afa8de0c2dd2121
Inna Vashchuk <vashchuk.inna@gmail.com>
2017-10-09 02:17:37 +0300

    lesson8

---
+++ src/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import authorization from './authorization';
+import router from './router';
+import Menu from './menu';
+
+class Init {
+    init = () => {
+        let menu = new Menu(router, authorization);
+        router.moveRouteChange = function (hash) {
+            menu.init();
+        };
+        menu.init();
+    };
+}
+
+(new Init()).init();
